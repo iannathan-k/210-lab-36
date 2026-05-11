b14b8bbf3b4c8d6d5426b6f6794a8a7831fe572b
iannathan-k <ianmtv@gmail.com>
2026-05-10 17:22:10 -0700

    milestone 1

--- main.cpp
+++ main.cpp
@@ -5,7 +5,15 @@ using namespace std;
 // COMSC-210 | Lab 36 | Ian Kusmiantoro
 
 int main() {
-    
+
+    cout << "Hello, World!" << endl;
+
+    IntBinaryTree tree = IntBinaryTree();
+    tree.insertNode(5);
+    tree.insertNode(3);
+    tree.insertNode(7);
+
+    tree.displayInOrder();
 
     return 0;
 }
